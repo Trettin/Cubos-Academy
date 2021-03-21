236aa9fb720e7623a9217caead86fd2642cb0934
Gabriel Trettin <gtcastro7@gmail.com>
2021-03-21 10:42:51 -0300

    back-m01-a04-casa-17 refatorado e adicionado casa 18-19-20

--- Back-end/back-m01-a04/m01-a04-casa-17/index.js
+++ Back-end/back-m01-a04/m01-a04-casa-17/index.js
@@ -1,12 +1,21 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
-let auxiliar = "";
+// let auxiliar = "";
+
+// while (filaDeDentro.length<5) {
+//     auxiliar = filaDeFora[0];
+//     filaDeFora.shift();
+//     filaDeDentro.push(auxiliar);
+// }
+// console.log(filaDeDentro);
+// console.log(filaDeFora);
+
 
 while (filaDeDentro.length<5) {
-    auxiliar = filaDeFora[0];
+    filaDeDentro.push(filaDeFora[0]);
     filaDeFora.shift();
-    filaDeDentro.push(auxiliar);
 }
+
 console.log(filaDeDentro);
 console.log(filaDeFora);
