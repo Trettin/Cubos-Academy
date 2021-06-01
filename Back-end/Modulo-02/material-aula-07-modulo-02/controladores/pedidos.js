a5b4b3beee714884c9f0878e8814528b64b4b55a
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-01 15:20:05 -0300

    material-aula-07-modulo-02

---
+++ Back-end/Modulo-02/material-aula-07-modulo-02/controladores/pedidos.js
@@ -0,0 +1,36 @@
+const instanciaAxios = require('../servicos/pagarme');
+
+const criarPedido = async (req, res) => {
+    const { body } = req;
+
+    try {
+        const pedido = await instanciaAxios.post('transactions', body);
+        return res.json(pedido.data);
+    } catch (error) {
+        const { data: { errors }, status } = error.response;
+
+        return res.status(status).json({
+            erro: `${errors[0].parameter_name} - ${errors[0].message}`
+        });
+    }
+}
+
+const consultarPedido = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const pedido = await instanciaAxios.get(`transactions/${id}`);
+        return res.json(pedido.data);
+    } catch (error) {
+        const { data: { errors }, status } = error.response;
+
+        return res.status(status).json({
+            erro: `${errors[0].parameter_name} - ${errors[0].message}`
+        });
+    }
+}
+
+module.exports = {
+    criarPedido,
+    consultarPedido
+}
