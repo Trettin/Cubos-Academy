1e89a70d31aadcf277cb818fa176ccce6247eb83
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-04 19:53:49 -0300

    atualizarUsuario fn implemented

--- Back-end/Modulo-03/back-conexao-node-postgres/classe-01/controladores/usuarios.js
+++ Back-end/Modulo-03/back-conexao-node-postgres/classe-01/controladores/usuarios.js
@@ -65,7 +65,52 @@ const cadastrarUsuario = async (req, res) => {
   }
 };
 
-const atualizarUsuario = async (req, res) => {};
+const atualizarUsuario = async (req, res) => {
+  const { id } = req.params;
+  const { nome, idade, email, telefone, cpf } = req.body;
+
+  try {
+    const usuario = await conexao.query(
+      "select * from usuarios where id = $1",
+      [id]
+    );
+
+    if (usuario.rowCount === 0) {
+      return res.status(404).json("Usuario não encontrado");
+    }
+
+    if (!nome) {
+      return res.status(400).json("O campo nome é obrigatório.");
+    }
+    if (!email) {
+      return res.status(400).json("O campo email é obrigatório.");
+    }
+    if (!cpf) {
+      return res.status(400).json("O campo cpf é obrigatório.");
+    }
+    if (String(cpf).length !== 11) {
+      return res.status(400).json("O cpf deve conter 11 digitos.");
+    }
+    const query =
+      "update usuarios set nome = $1, idade = $2, email = $3, telefone = $4, cpf = $5 where id = $6";
+    const usuarioAtualizado = await conexao.query(query, [
+      nome,
+      idade ?? null,
+      email,
+      telefone ?? null,
+      cpf,
+      id,
+    ]);
+
+    if (usuarioAtualizado.rowCount === 0) {
+      return res.status(404).json("Não foi possível atualizar o usuário.");
+    }
+
+    return res.status(200).json("Usuário foi atualizado com sucesso.");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const excluirUsuario = async (req, res) => {};
 
