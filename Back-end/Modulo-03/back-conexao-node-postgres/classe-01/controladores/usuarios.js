a1564a5a5a0ae6f9afdee34277c9e6265af86ee4
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-04 18:07:15 -0300

    listarUsuarios implemented

--- Back-end/Modulo-03/back-conexao-node-postgres/classe-01/controladores/usuarios.js
+++ Back-end/Modulo-03/back-conexao-node-postgres/classe-01/controladores/usuarios.js
@@ -1,6 +1,13 @@
 const conexao = require("../conexao");
 
-const listarUsuarios = async (req, res) => {};
+const listarUsuarios = async (req, res) => {
+  try {
+    const { rows: usuarios } = await conexao.query("select * from usuarios");
+    return res.status(200).json(usuarios);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const obterUsuario = async (req, res) => {};
 
