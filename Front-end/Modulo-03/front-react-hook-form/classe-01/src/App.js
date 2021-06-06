4c54791ec2efb29e354f772155c8c7a3824002aa
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-06 17:13:49 -0300

    Context, Find and DeleteComment implemented

--- Front-end/Modulo-03/front-react-hook-form/classe-01/src/App.js
+++ Front-end/Modulo-03/front-react-hook-form/classe-01/src/App.js
@@ -9,33 +9,41 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import NewComment from "./pages/NewComment";
+import FindComment from "./pages/FindComment";
+import DeleteComment from "./pages/DeleteComment";
+
+import ContextProvider from "./context/responseStateProvider";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <nav className="menu">
-          <NavLink to="/" exact className="navlink">
-            Home
-          </NavLink>
-          <NavLink to="/post" className="navlink">
-            New Comment
-          </NavLink>
-          <NavLink to="/get" className="navlink">
-            Find Comment
-          </NavLink>
-          <NavLink to="/put" className="navlink">
-            Update Comment
-          </NavLink>
-          <NavLink to="/delete" className="navlink">
-            Delete Comment
-          </NavLink>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/post" component={NewComment} />
-        </Switch>
-      </Router>
+      <ContextProvider>
+        <Router>
+          <nav className="menu">
+            <NavLink to="/" exact className="navlink">
+              Home
+            </NavLink>
+            <NavLink to="/post" className="navlink">
+              New Comment
+            </NavLink>
+            <NavLink to="/get" className="navlink">
+              Find Comment
+            </NavLink>
+            <NavLink to="/put" className="navlink">
+              Update Comment
+            </NavLink>
+            <NavLink to="/delete" className="navlink">
+              Delete Comment
+            </NavLink>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/post" component={NewComment} />
+            <Route path="/get" component={FindComment} />
+            <Route path="/delete" component={DeleteComment} />
+          </Switch>
+        </Router>
+      </ContextProvider>
     </div>
   );
 }
