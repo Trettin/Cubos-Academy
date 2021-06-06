7047d923e1732b878738b4ba1e1c5a67d9a7c44c
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-06 17:21:31 -0300

    FindComment page component

---
+++ Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/FindComment.js
@@ -0,0 +1,35 @@
+import Input from "../components/Input";
+import Error from "../components/Error";
+import Commentary from "../components/Commentary";
+
+import { useForm } from "react-hook-form";
+
+import useContextStates from "../hooks/useContextStates";
+
+export default function FindComment() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { onSubmitGet, findCommentResponse } = useContextStates();
+
+  return (
+    <form onSubmit={handleSubmit((data) => onSubmitGet(data))}>
+      <label htmlFor="input-findComment">
+        What is the id of the commentary you are looking for?
+        <Input
+          id="input-findComment"
+          type="number"
+          {...register("findId", { required: true })}
+        />
+        <Error errors={errors} errorName="commentId" />
+      </label>
+      <button type="submit">Send</button>
+      {findCommentResponse.title && (
+        <Commentary content={findCommentResponse} />
+      )}
+    </form>
+  );
+}
