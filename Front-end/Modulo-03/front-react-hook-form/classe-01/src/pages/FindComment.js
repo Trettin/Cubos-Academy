9e4cfe91c3e384fc13619b2f632dd507008579a9
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-06 19:24:10 -0300

    findComment now shows the response or form

--- Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/FindComment.js
+++ Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/FindComment.js
@@ -1,6 +1,6 @@
 import Input from "../components/Input";
 import Error from "../components/Error";
-import Commentary from "../components/Commentary/Commentary";
+import Commentary from "../components/Commentary/";
 
 import { useForm } from "react-hook-form";
 
@@ -16,20 +16,23 @@ export default function FindComment() {
   const { onSubmitGet, findCommentResponse } = useContextStates();
 
   return (
-    <form onSubmit={handleSubmit((data) => onSubmitGet(data))}>
-      <label htmlFor="input-findComment">
-        What is the id of the commentary you are looking for?
-        <Input
-          id="input-findComment"
-          type="number"
-          {...register("findId", { required: true })}
-        />
-        <Error errors={errors} errorName="commentId" />
-      </label>
-      <button type="submit">Send</button>
-      {findCommentResponse.title && (
+    <div>
+      {findCommentResponse.title ? (
         <Commentary content={findCommentResponse} />
+      ) : (
+        <form onSubmit={handleSubmit((data) => onSubmitGet(data))}>
+          <label htmlFor="input-findComment">
+            What is the id of the commentary you are looking for?
+            <Input
+              id="input-findComment"
+              type="number"
+              {...register("findId", { required: true })}
+            />
+            <Error errors={errors} errorName="commentId" />
+          </label>
+          <button type="submit">Send</button>
+        </form>
       )}
-    </form>
+    </div>
   );
 }
