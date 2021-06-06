c5a77be1e5afd62b9de9222a5ca3d296260880af
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-06 19:42:10 -0300

    NewComment refactored

--- Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/NewComment.js
+++ Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/NewComment.js
@@ -11,47 +11,62 @@ export default function NewComment() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
-  const { onSubmitPost, newCommentResponse } = useContextStates();
+  const { onSubmitPost, newCommentResponse, setNewCommentResponse } =
+    useContextStates();
 
+  function resetNewComment() {
+    setNewCommentResponse(null);
+    reset({
+      title: "",
+      commentary: "",
+      userId: "",
+    });
+  }
   return (
-    <form onSubmit={handleSubmit((data) => onSubmitPost(data))}>
-      <label htmlFor="input-title">
-        Title
-        <Input
-          id="input-title"
-          type="text"
-          {...register("title", { required: true, minLength: 5 })}
-        />
-        <Error errors={errors} errorName="mandatory" />
-        <Error errors={errors} errorName="title" />
-      </label>
+    <div>
+      {newCommentResponse ? (
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <span style={{ color: "green" }}>Comment received!</span>
+          <button onClick={resetNewComment}>Post a new comment...</button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit((data) => onSubmitPost(data))}>
+          <label htmlFor="input-title">
+            Title
+            <Input
+              id="input-title"
+              type="text"
+              {...register("title", { required: true, minLength: 5 })}
+            />
+            <Error errors={errors} errorName="mandatory" />
+            <Error errors={errors} errorName="title" />
+          </label>
 
-      <label htmlFor="input-comentary">
-        Commentary
-        <TextArea
-          id="input-comentary"
-          {...register("commentary", { required: true, maxLength: 30 })}
-        />
-        <Error errors={errors} errorName="commentary" />
-      </label>
+          <label htmlFor="input-comentary">
+            Commentary
+            <TextArea
+              id="input-comentary"
+              {...register("commentary", { required: true, maxLength: 30 })}
+            />
+            <Error errors={errors} errorName="commentary" />
+          </label>
 
-      <label htmlFor="userId">
-        User id
-        <Input
-          id="userId"
-          type="number"
-          {...register("userId", { required: true })}
-        />
-        <Error errors={errors} errorName="userId" />
-      </label>
+          <label htmlFor="userId">
+            User id
+            <Input
+              id="userId"
+              type="number"
+              {...register("userId", { required: true })}
+            />
+            <Error errors={errors} errorName="userId" />
+          </label>
 
-      <button type="submit">Send</button>
-
-      {newCommentResponse && (
-        <span style={{ color: "green" }}>Comment received!</span>
+          <button type="submit">Send</button>
+        </form>
       )}
-    </form>
+    </div>
   );
 }
