07b3333e6630655d466357aa3ab2a5e49cfc7681
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-06 20:06:07 -0300

    new logic on delete for reseting input

--- Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/DeleteComment.js
+++ Front-end/Modulo-03/front-react-hook-form/classe-01/src/pages/DeleteComment.js
@@ -10,25 +10,41 @@ export default function DeleteComment() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
-  const { onSubmitDelete, deleteResponse } = useContextStates();
+  const { onSubmitDelete, deleteResponse, setDeleteResponse } =
+    useContextStates();
+
+  function resetDelete() {
+    setDeleteResponse(false);
+    reset({
+      delete: "",
+    });
+  }
 
   return (
-    <form onSubmit={handleSubmit((data) => onSubmitDelete(data))}>
-      <label htmlFor="input-delete">
-        What is the id of the commentary you want to delete?
-        <Input
-          id="input-delete"
-          type="number"
-          {...register("delete", { required: true })}
-        />
-        <Error errors={errors} errorName="delete" />
-      </label>
-      <button type="submit">Send</button>
-      {deleteResponse && (
-        <span style={{ color: "green" }}>Comment deleted!</span>
+    <div>
+      {deleteResponse ? (
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <span style={{ color: "green" }}>Comment deleted!</span>
+
+          <button onClick={resetDelete}>Post a new comment...</button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit((data) => onSubmitDelete(data))}>
+          <label htmlFor="input-delete">
+            What is the id of the commentary you want to delete?
+            <Input
+              id="input-delete"
+              type="number"
+              {...register("delete", { required: true })}
+            />
+            <Error errors={errors} errorName="delete" />
+          </label>
+          <button type="submit">Send</button>
+        </form>
       )}
-    </form>
+    </div>
   );
 }
