c633409077f1847cf0e0d40240fa270ec77eee52
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-08 19:43:02 -0300

    values for context

--- Front-end/Modulo-03/front-design-system-and-libs/classe-01/src/hooks/contextValues.js
+++ Front-end/Modulo-03/front-design-system-and-libs/classe-01/src/hooks/contextValues.js
@@ -1,15 +1,99 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocalStorage } from "react-use";
 
 export default function useContextStates() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState({});
+  const [inputValue, setInputValue] = useState("");
+  const [pokemonsInStorage, setPokemonInStorage] = useLocalStorage(
+    "pokemons",
+    []
+  );
+  const [open, setOpen] = useState(false);
 
-  const onSubmitPost = async (data) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/Bulbasaur`);
-    const poke = await response.json();
-    setPokemon();
+  useEffect(async () => {
+    const pikachuInLocalStorage = pokemonsInStorage.find(
+      (poke) => poke.name === "pikachu"
+    );
+    if (pikachuInLocalStorage) return setPokemon(pikachuInLocalStorage);
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+      const {
+        name,
+        sprites: {
+          other: {
+            dream_world: { front_default },
+          },
+        },
+        abilities,
+      } = await response.json();
+      const currentPokemon = {
+        name,
+        abilities,
+        image: front_default,
+      };
+      setPokemon(currentPokemon);
+      setPokemonInStorage([...pokemonsInStorage, currentPokemon]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setOpen(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, open);
+
+  function checkInCache() {
+    return pokemonsInStorage.find(
+      (poke) => poke.name === inputValue.toLocaleLowerCase()
+    );
+  }
+
+  async function handleFindPokemon(e) {
+    e.preventDefault();
+    const result = checkInCache();
+    if (result) return setPokemon(result);
+
+    await onSubmit();
+  }
+
+  const onSubmit = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${inputValue}`
+      );
+      const {
+        name,
+        sprites: {
+          other: {
+            dream_world: { front_default },
+          },
+        },
+        abilities,
+      } = await response.json();
+      const currentPokemon = {
+        name,
+        abilities,
+        image: front_default,
+      };
+      setPokemon(currentPokemon);
+      setPokemonInStorage([...pokemonsInStorage, currentPokemon]);
+    } catch (error) {
+      console.log(error);
+      setOpen(true);
+    }
   };
 
   return {
-    onSubmitPost,
+    pokemon,
+    inputValue,
+    setInputValue,
+    handleFindPokemon,
+    open,
   };
 }
