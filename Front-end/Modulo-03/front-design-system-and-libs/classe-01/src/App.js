4e112c5826bf3dc03a1e862a3f8c8122da15d801
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-07 22:53:33 -0300

    app: header and input

--- Front-end/Modulo-03/front-design-system-and-libs/classe-01/src/App.js
+++ Front-end/Modulo-03/front-design-system-and-libs/classe-01/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import "@fontsource/roboto";
+import Header from "./components/Header";
+import Input from "./components/Input";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Input />
+    </div>
+  );
 }
 
 export default App;
