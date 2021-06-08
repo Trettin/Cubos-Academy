d0ce518186ca0123e3fd1245d03cddef25191c57
Gabriel Trettin <gtcastro7@gmail.com>
2021-06-07 22:52:49 -0300

    comp Header and style

---
+++ Front-end/Modulo-03/front-design-system-and-libs/classe-01/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import { Typography } from "@material-ui/core";
+
+export default function Header() {
+  return (
+    <header>
+      <Typography variant="h4">PokeDex</Typography>
+    </header>
+  );
+}
